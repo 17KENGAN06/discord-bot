e6865584d607f1e9cc1e6bc6539d8144ebfce2d7
KENGAN <kengangenkay@gmail.com>
2026-03-31 15:09:35 +0300

    next fix

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const { REST, Routes } = require('discord.js');
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
 await rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] });
 
 console.log('🧹 Все команды удалены');
